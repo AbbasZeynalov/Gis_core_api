a13948b1a7bfd9ed048b22768631b3b8cdf85ee4
JavidM <javidm@risk.local>
2019-10-07 10:51:25 +0400

    delete ormconfig.json

--- src/App.ts
+++ src/App.ts
@@ -20,7 +20,28 @@ class App {
 
         this.config();
 
-        await createConnection();
+        await createConnection({
+            "type": "mysql",
+            "host": "localhost",
+            "port": 3306,
+            "username": "root",
+            "password": "",
+            "database": "gis_core",
+            "synchronize": true,
+            "logging": false,
+            "entities": [
+                "src/entity/**/*.ts"
+            ],
+            "migrations": [
+                "src/migration/**/*.ts"
+            ],
+            "subscribers": [
+                "src/subscriber/**/*.ts"
+            ],
+            "cli": {
+                "migrationsDir": "src/migration"
+            }
+        });
 
         this.app.use('/graphql', cors(), GraphqlHttp());
     }
